f3c4747dc4e91887c731a0979878965db1ca54d2
Mosh Feu <moshfeu@gmail.com>
2019-08-31 22:42:52 +0300

    show notification from the service worker

--- sw.js
+++ sw.js
@@ -5,8 +5,19 @@ function startListen() {
     console.log(Date.now());
   }, 1000);
 }
+
 function stopListen() {
   clearInterval(interval);
+}
+
+function sendTestNotification() {
+  setTimeout(() => {
+    self.registration.showNotification(`I'm a notification from the service worker!!`, {
+      body: 'How cool is that?',
+      icon: '',
+      tag: ''
+    });
+  }, 5000);
 }
 
 self.addEventListener('message', ({data}) => {
@@ -16,6 +27,9 @@ self.addEventListener('message', ({data}) => {
       break;
     case 'stop':
       stopListen();
+      break;
+    case 'notification':
+      sendTestNotification();
       break;
   }
 });
