1e4e81d1f2a8444dc728bc5e96544ea79cf52378
Mosh Feu <moshfeu@gmail.com>
2019-08-31 21:44:44 +0300

    start / stop basic polling

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
+let interval;
+
+function startListen() {
+  interval = setInterval(() => {
+    console.log(Date.now());
+  }, 1000);
+}
+function stopListen() {
+  clearInterval(interval);
+}
+
 self.addEventListener('message', ({data}) => {
-  console.log(data);
+  switch (data) {
+    case 'start':
+      startListen();
+      break;
+    case 'stop':
+      stopListen();
+      break;
+  }
 });
